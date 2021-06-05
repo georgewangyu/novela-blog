4cc01b66798118fce9982ef705fcaedf6f3f3a2a
georgewangyu <gywang912@gmail.com>
2021-06-05 11:24:53 -0700

    completed W1 blog

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ * Logo comes with a property value called `fill`. `fill` is useful 
+ * when you want to change your logo depending on the theme you are on. 
+ */
+export default function Logo({ fill }) {
+  return (
+    <svg viewBox="0 0 510 510" height="30px">
+      <path d="M286.875,229.5v63.75h150.45c-15.3,89.25-86.7,153-175.95,153c-104.55,0-191.25-86.7-191.25-191.25
+			s86.7-191.25,191.25-191.25c53.55,0,99.45,22.95,132.6,58.65l45.9-45.9c-45.9-45.9-107.1-76.5-178.5-76.5
+			c-140.25,0-255,114.75-255,255s114.75,255,255,255s242.25-114.75,242.25-255v-25.5H286.875z" />
+    </svg>
+  );
+};
